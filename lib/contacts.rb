051e23a0015d865804d1cb51a2bc866e63041afd
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-16 05:49:27 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -32,7 +32,7 @@ def remove_strawberry(contacts)
     if person == "Freddy Mercury"
       contact_details_hash.each do
         |attribute, data|
-    binding.pry
+     binding.pry
   end
 end
 end
